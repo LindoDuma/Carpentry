620f55c49e5759180808958ec72659fd4c06c60a
Lindokuhle Duma <lindoduma10@gmail.com>
2022-08-10 20:21:45 +0200

    Add project files.

---
+++ Capentry/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Capentry.MvcApplication" Language="C#" %>
---
+++ Capentry/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Capentry.Startup))]
+namespace Capentry
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
