57b67d9bde94c43012dfd63fa5601cb7063f2941
Lindokuhle Duma <lindoduma10@gmail.com>
2022-08-21 17:10:12 +0200

    Updated packages including bootstrap

---
+++ Capentry/Models/Image.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace Capentry.Models
+{   
+    public class Image
+    {
+        [Key]
+        public int ImageID { get; set; }
+        public string ImageName { get; set; }
+        public string ImagePath{ get; set; }
+
+        //public Project Project { get; set; }
+    }
+}
