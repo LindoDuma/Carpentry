16d710b2adedada02d4ff84cde4060eecac173d4
Lindokuhle Duma <lindoduma10@gmail.com>
2023-01-25 18:36:23 +0200

    working contact form and home page animation

--- Capentry/Controllers/HomeController.cs
+++ Capentry/Controllers/HomeController.cs
@@ -4,6 +4,12 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Capentry.Controllers;
+using Capentry.ViewModels;
+using System.Net;
+using System.Net.Mail;
+using System.Configuration;
+using System.Net.Configuration;
+using System.IO;
 
 namespace Capentry.Controllers
 {
@@ -24,6 +30,45 @@ namespace Capentry.Controllers
         public ActionResult Contact()
         {
             ViewBag.Message = "";
+
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult ContactForm() 
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult ContactForm(ContactViewModel model) 
+        {
+            //Read SMTP section from Web.Config.
+            SmtpSection smtpSection = (SmtpSection)ConfigurationManager.GetSection("system.net/mailSettings/smtp");
+
+            using (MailMessage mm = new MailMessage(smtpSection.From, "admin@aspsnippets.com"))
+            {
+                mm.Subject = model.Subject;
+                mm.Body = "Name: " + model.Name + "<br /><br />Email: " + model.Email + "<br />" + model.Body;
+                //if (model.Attachment.ContentLength > 0)
+                //{
+                //    string fileName = Path.GetFileName(model.Attachment.FileName);
+                //    mm.Attachments.Add(new Attachment(model.Attachment.InputStream, fileName));
+                //}
+                mm.IsBodyHtml = true;
+
+                using (SmtpClient smtp = new SmtpClient())
+                {
+                    smtp.Host = smtpSection.Network.Host;
+                    smtp.EnableSsl = smtpSection.Network.EnableSsl;
+                    NetworkCredential networkCred = new NetworkCredential(smtpSection.Network.UserName, smtpSection.Network.Password);
+                    smtp.UseDefaultCredentials = true;
+                    smtp.Credentials = networkCred;
+                    smtp.Port = smtpSection.Network.Port;
+                    smtp.Send(mm);
+                    ViewBag.Message = "Email sent sucessfully.";
+                }
+            }
 
             return View();
         }
---
+++ Capentry/ViewModels/ContactViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Net;
+using System.Net.Mail;
+using System.Configuration;
+using System.Net.Configuration;
+using System.Web.Mvc;
+
+namespace Capentry.ViewModels
+{
+    public class ContactViewModel
+    {
+        public string Name { get; set; }
+        public string Subject { get; set; }
+        public string Email { get; set; }
+        [AllowHtml]
+        public string Body { get; set; }
+        public HttpPostedFileBase Attachment { get; set; }
+    }
+}
