648820e81e922c9d7470b395f2668838d4b991c9
Lindokuhle Duma <lindoduma10@gmail.com>
2023-04-03 19:02:37 +0200

    confirm email page redone

--- Capentry/Controllers/AccountController.cs
+++ Capentry/Controllers/AccountController.cs
@@ -215,12 +215,13 @@ namespace Capentry.Controllers
         [AllowAnonymous]
         public async Task<ActionResult> ConfirmEmail(string userId, string code)
         {
-            if (userId == null || code == null)
-            {
-                return View("Error");
-            }
-            var result = await UserManager.ConfirmEmailAsync(userId, code);
-            return View(result.Succeeded ? "ConfirmEmail" : "Error");
+            //if (userId == null || code == null)
+            //{
+            //    return View("Error");
+            //}
+            //var result = await UserManager.ConfirmEmailAsync(userId, code);
+            //return View(result.Succeeded ? "ConfirmEmail" : "Error");
+            return View();
         }
 
         //
